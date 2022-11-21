64305d32f01e565bfa8ed7c251f9dc972f5f6f08
danny-pye <danny.luke.pye@gmail.com>
2022-11-21 21:36:47 +0000

    done

--- app/src/main/java/com/example/age_in_minutes/MainActivity.kt
+++ app/src/main/java/com/example/age_in_minutes/MainActivity.kt
@@ -12,6 +12,7 @@ import java.util.*
 class MainActivity : AppCompatActivity() {
 
     private var dateSelected: TextView? = null
+    private var minutesDisplay: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,20 +20,22 @@ class MainActivity : AppCompatActivity() {
 
         val datePicker: Button = findViewById(R.id.datePickerButton)
         dateSelected = findViewById(R.id.displayDate)
+        minutesDisplay = findViewById(R.id.textMinutesView)
         datePicker.setOnClickListener {
             datePickerHandler()
         }
     }
 
-    fun datePickerHandler(){
+    private fun datePickerHandler(){
         val myCalendar = Calendar.getInstance()
         val year = myCalendar.get(Calendar.YEAR)
         val month = myCalendar.get(Calendar.MONTH)
         val day = myCalendar.get(Calendar.DAY_OF_MONTH)
 
-        DatePickerDialog(
+
+        val dpd = DatePickerDialog(
             this,
-            DatePickerDialog.OnDateSetListener{view, selectedYear, selectedMonth, selectedDayOfMonth ->
+            DatePickerDialog.OnDateSetListener{_, selectedYear, selectedMonth, selectedDayOfMonth ->
 //                Toast.makeText(this, "$selectedDayOfMonth / ${selectedMonth + 1} / $selectedYear", Toast.LENGTH_LONG).show()
                 val theDate = "$selectedDayOfMonth/${selectedMonth + 1}/$selectedYear"
                 dateSelected?.text = theDate
@@ -40,11 +43,28 @@ class MainActivity : AppCompatActivity() {
                 val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
 
                 val sdfDate = sdf.parse(theDate)
+                sdfDate?.let{
+                    val selectedDateInMinutes = sdfDate.time / 60000
+
+                    val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
+
+                    currentDate?.let{
+                        val currentDateInMinutes = currentDate.time / 60000
+
+                        val difference = currentDateInMinutes - selectedDateInMinutes
+
+                        minutesDisplay?.text = difference.toString()
+                    }
+
+
+                }
 
             },
             year, month, day
 
-        ).show()
+        )
+        dpd.datePicker.maxDate = System.currentTimeMillis()
+        dpd.show()
 //        Toast.makeText(this, "$day $month $year", Toast.LENGTH_LONG).show()
 
     }
